0d3d669eda9978f6e3631c868bbcebc68bf05a6c
RIFKI ALFATAH <rifkialfatah@student.telkomuniversity.ac.id>
2026-05-10 21:47:14 +0700

    AljabarLibraries

---
+++ AljabarLibraries/AljabarLibraries.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double diskriminan = b * b - 4 * a * c;
+
+            double x1 = (-b + Math.Sqrt(diskriminan)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(diskriminan)) / (2 * a);
+
+            return new double[] { x1, x2 };
+        }
+
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            return new double[]
+            {
+                a * a,
+                2 * a * b,
+                b * b
+            };
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using AljabarLibraries;
+
+double[] akar = Aljabar.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+
+Console.WriteLine("Akar Persamaan Kuadrat:");
+Console.WriteLine($"Output: {{ {akar[0]}, {akar[1]} }}");
+
+double[] hasil = Aljabar.HasilKuadrat(new double[] { 2, -3 });
+
+Console.WriteLine();
+Console.WriteLine("Hasil Kuadrat:");
+Console.WriteLine($"Output: {{ {hasil[0]}, {hasil[1]}, {hasil[2]} }}");
